cdac26c6f45e2b766c3e34c871f2b27d7fad9696
ezzatisawesome <ezzatsuhaime@gmail.com>
2025-04-02 00:46:29 -0700

    Adding more mass stuff

--- optimize.py
+++ optimize.py
@@ -16,23 +16,35 @@ opti = asb.Opti(
 )
 
 ### CONSTANTS
+## Mass & mass related
 # Power 
-liion_21700_cell_m = 70 # grams
-solar_panel_with_busbar_solder = 15 # grams
+liion_21700_cell_mass = 70 # grams
+solar_panel_with_busbar_solder_mass = 15 # grams
+mppt_mass = 200 # grams
 
 # Avionics
-speedybee_f405_m = 55 # grams
-gps_bn880_m = 12 # grams
-telemtry_with_wire = 26 # grams
-radio_receiver_with_wire = 18 # grams
+speedybee_f405_mass = 55 # grams
+gps_bn880_mass = 12 # grams
+telemtry_with_wire_mass = 26 # grams
+radio_receiver_with_wire_mass = 18 # grams
+wiring_mass = 50 # grams
+servos_mass = 50 # grams
 
 # Propulsion
-esc = 25 # ! grams, may change ESC
-motor_410kv = 277 # grams
+esc_mass = 25 # ! grams, may change ESC
+motor_410kv_mass = 277 # grams
+propeller_mass = 52 # grams
 
 # Structures
-carbon_22_per_meter = 90 # grams / m
-carbon_20_per_meter = 80 # grams / m
+fuselage_mass = 100 # grams
+motor_pods_mass = 60 # grams
+carbon_22_per_meter_mass = 90 # grams / m
+carbon_20_per_meter_mass = 80 # grams / m
+
+# Wing
+ngx_foam_density = 30.02 # kg / m^3
+
+
 
 
 # PARAMETERS
@@ -43,6 +55,7 @@ n_panels_chordwise_outboard = opti.parameter(1)
 
 
 
+
 # MASS
 design_mass_TOGW = opti.variable(
     init_guess=5,
@@ -50,6 +63,9 @@ design_mass_TOGW = opti.variable(
 )
 
 
+# get # panels for certain power
+
+# get # batteries for certain power
 
 
 
